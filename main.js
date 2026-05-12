8976d080a4ada688fee02c51d276010fd8a63714
Dante Orti <uarjoldanteorti@gmail.com>
2026-05-12 15:52:22 -0300

    tarifa undefined¯\_(ツ)_/¯

--- main.js
+++ main.js
@@ -8,6 +8,46 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
 
+
+
+
 boton.addEventListener("click", () => {
-  // COMPLETAR
+  let dist;
+  let reg;
+  let precio;
+
+  reg = registrada.value;
+  dist = distancia.value;
+  tarifa.innerText = precio;
+
+  if(reg === "si"){
+    if(dist <= 3){
+      precio = 715.24
+    }
+    else if(dist <= 6){
+      precio = 797.74
+    }
+    else if(dist <= 12){
+      precio = 855.97
+    }
+    else if(dist > 12){
+      precio = 917.24
+    }
+  };
+  if(reg === "no"){
+    if(dist <= 3){
+      precio = 1137.23
+    }
+    else if(dist <= 6){
+      precio = 1263.64
+    }
+    else if(dist <= 12){
+      precio = 1360.99
+    }
+    else if(dist > 12){
+      precio = 1458.41
+    }
+  };
 });
+
+
